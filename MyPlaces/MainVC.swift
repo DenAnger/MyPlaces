187ab434997251a2f45e535fdbcb6a1a1962580c
Denis Abramov <denmiracle@mail.ru>
2020-06-26 16:40:01 +0700

    Sum
    Init

---
+++ MyPlaces/MainVC.swift
@@ -0,0 +1,26 @@
+//
+//  MainVC.swift
+//  MyPlaces
+//
+//  Created by Denis Abramov on 22.06.2020.
+//  Copyright © 2020 Denis Abramov. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
