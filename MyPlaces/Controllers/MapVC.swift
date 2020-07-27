05ac21d8800421ece4382e63c8757d09e8621ddd
Denis Abramov <denmiracle@mail.ru>
2020-07-27 12:56:20 +0700

    Add Map view controller

---
+++ MyPlaces/Controllers/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  MyPlaces
+//
+//  Created by Denis Abramov on 27.07.2020.
+//  Copyright © 2020 Denis Abramov. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeVC(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
